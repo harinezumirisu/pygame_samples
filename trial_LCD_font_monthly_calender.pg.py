3c2a09b425d21800d220de533b02194cf999e7d1
aotearoa <128959102+aoteaeoa@users.noreply.github.com+aotearoa@users.noreply.github.com>
2024-02-04 17:53:09 +0900

    修正

--- trial_LCD_font_monthly_calender.pg.py
+++ trial_LCD_font_monthly_calender.pg.py
@@ -45,12 +45,9 @@ def getMonthDays():
             return  28
 
 
-#　曜日の取得
+#　曜日の取得(ツェラーの公式、この場合日曜日が0になる)
 def getWeekDay(y, m, d):
-    y = dt_now.year
-    m = dt_now.month
-    d = dt_now.day
-    if dt_now.month == 1 or dt_now.month == 2:
+    if m == 1 or m == 2:
         y -= 1
         m += 12
     w = (y + y // 4 - y // 100 + y // 400 + (13 * m + 8) // 5 + d) % 7
@@ -77,20 +74,20 @@ z = getMonthDays()
 wd = getWeekDay(dt_now.year, dt_now.month, 1)
 
 for n in range(z):  # nはcol2行分で各日にちを表し、各月の日数分であるｚ回繰り返して日付表示する
-    x =3 + ((3 * n + 2) + (wd + 1) % 7 * 3)// 21
+    x =3 + ((3 * n + 2) + wd * 3)// 21
     lcdx = LCD_font_pg(screen)
     lcdx.init_col(BLOCK_SIZE=7, BLOCK_INTV=8, COLOR_ON=WHITE, COLOR_OFF=ORANGE)
     lcdx.init_row(X_ORG=8, Y_ORG=(8 + (x - 1) * 10), COL_INTV=6)
-    lcdx.update_col(col=((3 * n + 1) + (wd + 1) % 7 * 3)  % 21, code=(n + 1) // 10) # カレンダー各日10の位
-    lcdx.update_col(col=((3 * n + 2) + (wd + 1) % 7 * 3 ) % 21, code=((n + 1) % 10)) # カレンダー各日1の位
+    lcdx.update_col(col=((3 * n + 1) + wd * 3)  % 21, code=(n + 1) // 10) # カレンダー各日10の位
+    lcdx.update_col(col=((3 * n + 2) + wd * 3 ) % 21, code=((n + 1) % 10)) # カレンダー各日1の位
 
 o = dt_now.day -1  # 今日の日付の色を変更する
-xo =3 + ((3 * o + 2) + (wd + 1) % 7 * 3)// 21
+xo =3 + ((3 * o + 2) + wd * 3)// 21
 lcdxo = LCD_font_pg(screen)
 lcdxo.init_col(BLOCK_SIZE=7, BLOCK_INTV=8, COLOR_ON=RED, COLOR_OFF=ORANGE)
 lcdxo.init_row(X_ORG=8, Y_ORG=(8 + (xo - 1) * 10), COL_INTV=6)
-lcdxo.update_col(col=((3 * o + 1) + (wd + 1) % 7 * 3)  % 21, code=(o + 1) // 10) # カレンダー各日10の位
-lcdxo.update_col(col=((3 * o + 2) + (wd + 1) % 7 * 3 ) % 21, code=((o + 1) % 10)) # カレンダー各日1の位
+lcdxo.update_col(col=((3 * o + 1) + wd * 3)  % 21, code=(o + 1) // 10) # カレンダー各日10の位
+lcdxo.update_col(col=((3 * o + 2) + wd * 3 ) % 21, code=((o + 1) % 10)) # カレンダー各日1の位
 
 
 running = True
