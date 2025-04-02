60e7249e41ada3d0d4acb31508d1b3f5ef77e9de
harinezumirisu <99530408+harinezumirisu@users.noreply.github.com>
2025-04-02 09:19:03 +0900

    test

--- demo_06_alphabet.py
+++ demo_06_alphabet.py
@@ -6,12 +6,8 @@ import pygame
 import pygame
 from pygame.locals import Rect
 import pygame.freetype
-from seven_seg_pg import Seven_seg
-from mcje.minecraft import Minecraft
-import param_MCJE as param
 from lcd_font_pg_big import LCD_font as LCD_font_mc
 from lcd_font_pg import LCD_font as LCD_font_pg
-from lcd_font_pmc import LCD_font as LCD_font_pmc
 
 DARK_GRAY = (40, 40, 40)
 GRAY = (80, 80, 80)
@@ -51,28 +47,15 @@ screen = pygame.display.set_mode([400, 320])
 pygame.display.set_caption("pygame 7-segment display simulation")
 screen.fill(DARK_GRAY)
 
-lcd1 = LCD_font_mc(screen)
-lcd1.__init__(screen)
-lcd1.init_col(BLOCK_SIZE=7, BLOCK_INTV=8, COLOR_ON=GREEN, COLOR_OFF=DARK_GRAY)
-lcd1.init_row(X_ORG=2, Y_ORG=35, COL_INTV=6)
-
 display1 = LCD_font_mc(screen)
 display1.__init__(screen)
 display1.init_col(BLOCK_SIZE=5, BLOCK_INTV=7, COLOR_ON=GREEN, COLOR_OFF=GRAY)
-display1.init_row(X_ORG=2, Y_ORG=21, COL_INTV=6)
-
-display3 = Seven_seg(screen)
-display3.init_col(BLOCK_SIZE=5, BLOCK_INTV=7, COLOR_ON=GREEN, COLOR_OFF=DARK_GRAY)
-display3.init_row(X_ORG=2, Y_ORG=28, COL_INTV=6)
+display1.init_row(X_ORG=2, Y_ORG=7, COL_INTV=6)   
 
 display2 = LCD_font_mc(screen)
 display2.__init__(screen)
-display2.init_col(BLOCK_SIZE=4, BLOCK_INTV=6, COLOR_ON=GREEN, COLOR_OFF=GRAY)
-display2.init_row(X_ORG=2, Y_ORG=7, COL_INTV=6)
-
-display4 = Seven_seg(screen)
-display4.init_col(BLOCK_SIZE=4, BLOCK_INTV=6, COLOR_ON=GREEN, COLOR_OFF=DARK_GRAY)
-display4.init_row(X_ORG=2, Y_ORG=14, COL_INTV=6)   
+display2.init_col(BLOCK_SIZE=5, BLOCK_INTV=7, COLOR_ON=GREEN, COLOR_OFF=GRAY)
+display2.init_row(X_ORG=2, Y_ORG=21, COL_INTV=6)
 
 running = True
 # infinite loop top ----
